16b0f908b2b5de614b48c50abed5996727b2f617
Giancarlo <orion2014@users.noreply.github.com>
2014-11-23 18:33:56 +0100

    makeCahceMatrix
    first function - test

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,41 @@
 ## The following couple of functions will calculate the inverse of a matrix
 ## The inverse will be cached so that it can be retrieved in case of need.
 
-## This function cretes a matrix that can store its inverse into a cache 
-## and gives the user a method to retrieve it
+## This function cretes an object that stores the matrix and its inverse 
+## there are 4 methods: two to read the values of the stored items, 
+## two to set the values of the stored items
 
 makeCacheMatrix <- function(x = matrix()) {
 
-   
-}
+## initialize the inverse matrix
+	inverse_cache <- NULL 
 
+## method to store the original matrix
+	set <- function(n) {
+			x <<- m
+			inverse_cache <<- NULL
+		}
+
+## method to retrieve the original matrix
+	get <- function() x
+
+## method to set the inverse matrix
+	storeInverse <- function(inverse) inverse_cache <<- inverse
+
+## method to retrieve the inverse matrix
+	retrieveInverse <- function() inverse_cache
+
+## list methods
+	list(set=set, get=get, storeInverse=storeInverse, retrieveInverse=retrieveInverse)
+
+}	
+  
+
+## metodo prova:
+## > x <- matrix(runif(16,0,100), nrow = 4, ncol= 4)
+## > test = makeCacheMatrix(x)
+## > test$get()
+## > x
 
 ## This function will search the cache for an inverse of the matrix "x"
 ## if the function is found in the cache, then this function will return the vaules stored in the cache
