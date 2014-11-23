42fb72d1d56a103bfa77afe3bc205481722b82ca
Giancarlo <orion2014@users.noreply.github.com>
2014-11-23 19:11:52 +0100

    makeCacheMatrix + cacheSolve
    completed

--- cachematrix.R
+++ cachematrix.R
@@ -32,17 +32,54 @@ makeCacheMatrix <- function(x = matrix()) {
   
 ## This function will search the cache for an inverse of the matrix "x"
 ## if the function is found in the cache, then this function will return the vaules stored in the cache
-## otherwise, the inverse will be calculated.
+## otherwise, the inverse will be calculated and then stored in the cache.
 
 cacheSolve <- function(x, ...) {
 
+## retrieve the inverse from the cache
+
+        inverse <- x$retrieveInverse()
+
+## if the inverse in the cache exists, then it will be returned and the function ends 
+	if( !is.null(inverse) ) {
+		message("getting cached data")
+		return(inverse)
+	}
+
+## Retrieve the original matrix
+	matrice_originale <- x$get()
+	
+## Calculate the inverse matrix 
+	inverse <- solve(matrice_originale)
+	
+## Store the result of the previous step
+	x$storeInverse(inverse)
+	
+## Return the calculated inverse matrix
+	inverse
 	
 }
 
 
-
 ## sample test:
-## > x <- matrix(runif(16,0,100), nrow = 4, ncol= 4)
-## > test = makeCacheMatrix(x)
-## > test$get()
-## > x
+## > originalMatrix <- matrix(sample(1:100,16), nrow = 4, ncol= 4)
+## > test = makeCacheMatrix(originalMatrix)
+## > cacheSolve(test)
+## > inverse <- cacheSolve(test)
+
+## the cached data has been retrieved
+
+## > test_inverse = makeCacheMatrix(inverse)
+## > inverseOfInverse <- cacheSolve(test_inverse)
+
+## the inverse is different, so it is calculated again
+
+## now we have a matrix that must be the same of the originalMatrix
+
+## > originalMatrix
+## > inverse
+## > inverseOfInverse
+
+## > cacheSolve(test_inverse)
+
+## will retrieved the cached data of the last calculated inverse
